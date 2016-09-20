59d2ac004e60dccffb96ae85ec71d3ba0529bd1b
Andrej Halaj <xhalaj1@nymfe53.fi.muni.cz>
2016-09-20 16:35:11 +0200

    first commmit

---
+++ projectPV165/src/main/java/cz/pv165/halaj/App.java
@@ -0,0 +1,13 @@
+package cz.pv165.halaj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello PV165!" );
+    }
+}
